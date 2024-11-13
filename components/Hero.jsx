947a4e8299befa0af7df0263bc85678dc1b5e58b
Luan <lr.prazeres@bol.com.br>
2024-11-13 16:48:20 -0300

    feat: hiddenSocials

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -24,12 +24,12 @@ const Hero = () => {
           <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
             <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">Desenvolvedor Front-End</div>
             <h1 className='h1'>Olá, me chamo Luan Rocha</h1>
-            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">Graduado em Análise e Desenvolvimento de Sistemas, possuo especialização em programação e gestão de projetos,
+            <p className="subtitle max-w-[440px] mx-auto xl:mx-0">Graduado em Análise e Desenvolvimento de Sistemas, possuo especialização em programação e gestão de projetos,
               destacando habilidades em HTML, CSS, JavaScript, PHP, Bootstrap e React Js, preparado para contribuir com soluções
               eficazes e inovadoras no campo da Tecnologia da Informação. Atualmente, atuo como Especialista em Suporte de TI,
               com experiência sólida na eficiente organização de cabos de rede e configuração avançada de switches. Destaco-me no
               atendimento a usuários, resolvendo questões de conectividade e desempenho de maneira proativa.</p>
-            <div className="flex flex-col gap-y-2 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto w-[162] xl:mx-0 mb-12">
               <Link href='/contact'>
                 <Button className='gap-x-2'><Send size={18} />
                   Entra em contato
@@ -41,10 +41,12 @@ const Hero = () => {
                 </Button>
               </Link>
             </div>
+            <div className="hidden sm:flex">
             <Socials containerStyles='flex gap-x-6  mx-auto xl:mx-0' iconsStyles='text-foreground text-[22px] hover:text-primary transition-all' />
+            </div>
           </div>
           {/* imagem */}
-          <div className="hidden flex-relative">imagem</div>
+          {/* <div className="hidden flex-relative">imagem</div> */}
         </div>
         {/* icon */}
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
